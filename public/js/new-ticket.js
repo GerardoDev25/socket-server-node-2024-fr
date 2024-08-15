6c74b86fb1d348f2ac74d7341cf61f513c5734d3
Gerardo Miranda <gerardodev25@gmail.com>
2024-08-14 20:03:43 -0400

    chore(dev): :construction: new ticker fron end screem done

--- public/js/new-ticket.js
+++ public/js/new-ticket.js
@@ -1,3 +1,18 @@
+const currentTickerLabel = document.getElementById('lbl-new-ticket');
+const createNewTicketButton = document.querySelector('button');
 
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('/api/tickets/last')
+    .then((res) => res.json())
+    .then((res) => (currentTickerLabel.textContent = res))
+    .catch((error) => console.error(error));
+});
 
-console.log('Nuevo Ticket HTML');
+createNewTicketButton.addEventListener('click', createNewTicket);
+
+function createNewTicket() {
+  fetch('/api/tickets/', { method: 'POST' })
+    .then((res) => res.json())
+    .then((res) => (currentTickerLabel.textContent = res.number))
+    .catch((error) => console.error(error));
+}
